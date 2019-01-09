28dedf9e5a0991b454bd376a0f1383b30ccaac33
elvardzelashvili <45464513+elvardzelashvili@users.noreply.github.com>
2019-01-09 12:42:02 -0800

    try webkit auto

--- gulpfile.js
+++ gulpfile.js
@@ -16,14 +16,3 @@ gulp.task('watch', function () {
 });
 
 
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
- 
-gulp.task('default', () =>
-    gulp.src('src/app.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'))
-);
