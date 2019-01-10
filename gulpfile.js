bccdd134a2319b798dc1f1a28fe81adf95525ea8
elvardzelashvili <45464513+elvardzelashvili@users.noreply.github.com>
2019-01-10 07:50:49 -0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+const autoprefixer = require('gulp-autoprefixer');
+
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 6 versions'],
+      cascade: false
+  }))
     .pipe(gulp.dest('styles'));
 });
  
